9d8c10039b0d90df616e187ea47bebd6c794a179
maorRoz <andory@post.bgu.ac.il>
2019-07-27 22:57:50 +0300

    removed space

--- src/types/server/Urls.fakes.ts
+++ src/types/server/Urls.fakes.ts
@@ -1,7 +1,5 @@
-
 import { Urls } from './Urls';
 import { Fakes } from '../../../tests/Fakes';
-
 export const FakeUrls = (props?: Urls): Urls => ({
   local: Fakes.string(),
   production: Fakes.string(),
