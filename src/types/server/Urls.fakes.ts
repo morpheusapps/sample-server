9b47ca47d5066fc0e0f78196ad9f48ef36e9542c
maorRoz <andory@post.bgu.ac.il>
2019-07-27 22:58:17 +0300

    added space

--- src/types/server/Urls.fakes.ts
+++ src/types/server/Urls.fakes.ts
@@ -1,5 +1,6 @@
 import { Urls } from './Urls';
 import { Fakes } from '../../../tests/Fakes';
+
 export const FakeUrls = (props?: Urls): Urls => ({
   local: Fakes.string(),
   production: Fakes.string(),
